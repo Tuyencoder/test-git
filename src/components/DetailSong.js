505f83f8b0eeb880de21b9ceb142bf28c2f9f682
Tuyencoder <xuantuyenq@gmail.com>
2023-04-18 19:08:13 +0700

    update

--- src/components/DetailSong.js
+++ src/components/DetailSong.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import Songs from '../Songs'
 export default function DetailSong() {
   const {song} =  useContext(Songs)
+  
   return (
     <div className='col-span-1 text-start text-slate-400 p-3'>
         <h2 className='font-bold text-[#38BDF8]'>Now Playing</h2>
@@ -10,7 +11,7 @@ export default function DetailSong() {
             <img src={song.links.images[0].url} alt='alan-woker' />
         </div>
         <div className='flex justify-evenly items-center mt-10'>  
-        <img className='w-[70px] rounded-full' src={song.links.images[1].url} alt='alan-woker' />
+        <img  className=' w-[70px] rounded-full' src={song.links.images[1].url} alt={song.author} />
         <span className='text-white'>{song.author}</span>
         </div>
     </div>
--- src/components/List.js
+++ src/components/List.js
@@ -7,11 +7,15 @@ export default function List() {
   const handlePlaysong =(idsong) =>{
     setIdsong(idsong)
     hanleSetSong(idsong)
+ 
+    
+      
+    
   }
+ 
   useEffect(() => {
     setIdsong(song.id)
   }, [song])
-  
   return (
     <div className='col-span-2 overflow-y-scroll ' >
       <table className='table-auto w-full '>
@@ -26,9 +30,8 @@ export default function List() {
         <tbody >
           {
             Datasongs.map((song, index) => {
-              
               return (
-                <tr key={index} className={`text-[#767D87] bg-slate-800 text-xl  hover:bg-[#475569] hover:cursor-pointer ${idsong === song.id  && 'text-teal-400  bg-[#475569]'   } `}
+                <tr key={index} className={`text-[#767D87] bg-slate-800 text-xl  hover:bg-[#475569] hover:cursor-pointer ${idsong === song.id  && 'text-teal-400  bg-[#475569]'     } `}
                 onClick={()=>handlePlaysong(song.id)}
                 >
                   <td className='text-end'>{song.id}</td>
